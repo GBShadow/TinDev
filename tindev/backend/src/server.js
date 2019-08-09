53317a1350042666f23e3274476537eb63597f88
GBShadow <gbs.shadow@gmail.com>
2019-08-09 18:06:01 -0300

    Adcionando o Projeto do TinDev

---
+++ tindev/backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect("mongodb+srv://gbshadow:gbs123@cluster0-gpjn6.mongodb.net/omnistack8?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+});
+
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333, () => {
+    console.log('Servidor Rodoando');
+});
