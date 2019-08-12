19c4916caf57290f4e9a8ceaf075edf56d2fd657
GBShadow <gbs.shadow@gmail.com>
2019-08-12 18:28:58 -0300

    Opção de Captar e verificar os dados de usuario

--- tindev/backend/src/controllers/DevController.js
+++ tindev/backend/src/controllers/DevController.js
@@ -1,13 +1,27 @@
 const axios = require('axios');
+const Dev = require('../models/Dev');
 
 module.exports = {
-    store(req, res) {
+    async store(req, res) {
         const { username } = req.body;
+
+        const userExistis = await Dev.findOne({user: username});
+
+        if(userExistis){
+            return res.json(userExistis);
+        }
 
         const response = await axios.get(`https://api.github.com/users/${username}`);
 
-        console.log(response.data);
+        const {name, bio, avatar_url: avatar} = response.data;
 
-        return res.json({ok: true})
+        const dev = await Dev.create({
+            name,
+            user: username,
+            bio,
+            avatar
+        })
+
+        return res.json(dev);
     }
 };
